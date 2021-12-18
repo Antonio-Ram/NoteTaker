3c6ce7f7623cff79d16c1ba9ee1b65b5271ede06
Antonio <tonito@umich.edu>
2021-12-18 12:44:52 -0500

    Coming together

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const { notes } = require ('./db/notes');
+const fs = require('fs');
+const path = require('path');
+const { notes } = require ('./db/db');
 
 const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query)
     res.json(results);
+});
+
+function validateNote(note) {
+    if(!note.title || typeof note.title !=='string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !=='string') {
+        return false;
+    }
+    return true;
+}
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    }else{
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 app.listen(3001, () => {
